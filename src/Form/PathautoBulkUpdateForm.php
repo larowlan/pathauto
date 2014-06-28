672f6373e5f97c3d843719e5c3ed58e06a47a1f8
Sascha Grossenbacher <saschagros@gmail.com>
2014-06-28 15:58:36 +0200

    Remove admin.inc, fix bulk update tests

--- src/Form/PathautoBulkUpdateForm.php
+++ src/Form/PathautoBulkUpdateForm.php
@@ -2,17 +2,17 @@
 
 /**
  * @file
- * Contains \Drupal\pathauto\Form\MaillogSettingsForm.
+ * Contains \Drupal\pathauto\Form\PathautoBulkUpdateForm.
  */
 
 namespace Drupal\pathauto\Form;
 
-use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormBase;
 
 /**
  * Configure file system settings for this site.
  */
-class PathautoBulkUpdateForm extends ConfigFormBase {
+class PathautoBulkUpdateForm extends FormBase {
 
   /**
    * {@inheritdoc}
@@ -52,16 +52,75 @@ class PathautoBulkUpdateForm extends ConfigFormBase {
       '#value' => t('Update'),
     );
 
-    return parent::buildForm($form, $form_state);
+    return $form;
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, array &$form_state) {
+    $batch = array(
+      'title' => t('Bulk updating URL aliases'),
+      'operations' => array(
+        array('Drupal\pathauto\Form\PathautoBulkUpdateForm::batchStart', array()),
+      ),
+      'finished' => 'Drupal\pathauto\Form\PathautoBulkUpdateForm::batchFinished',
+    );
 
-    parent::submitForm($form, $form_state);
+    foreach ($form_state['values']['update'] as $callback) {
+      if (!empty($callback)) {
+        $settings = $form['#update_callbacks'][$callback];
+        if (!empty($settings->batch_file)) {
+          $batch['operations'][] = array('Drupal\pathauto\Form\PathautoBulkUpdateForm::batchProcess', array($callback, $settings));
+        }
+        else {
+          $batch['operations'][] = array($callback, array());
+        }
+      }
+    }
 
+    batch_set($batch);
+  }
+
+  /**
+   * Batch callback; count the current number of URL aliases for comparison later.
+   */
+  public static function batchStart(&$context) {
+    $context['results']['count_before'] = db_select('url_alias')->countQuery()->execute()->fetchField();
+  }
+
+  /**
+   * Common batch processing callback for all operations.
+   *
+   * Required to load our include the proper batch file.
+   */
+  public static function batchProcess($callback, $settings, &$context) {
+    if (!empty($settings->batch_file)) {
+      require_once DRUPAL_ROOT . '/' . $settings->batch_file;
+    }
+    return $callback($context);
+  }
+
+  /**
+   * Batch finished callback.
+   */
+  public static function batchFinished($success, $results, $operations) {
+    if ($success) {
+      // Count the current number of URL aliases after the batch is completed
+      // and compare to the count before the batch started.
+      $results['count_after'] = db_select('url_alias')->countQuery()->execute()->fetchField();
+      $results['count_changed'] = max($results['count_after'] - $results['count_before'], 0);
+      if ($results['count_changed']) {
+        drupal_set_message(\Drupal::translation()->formatPlural($results['count_changed'], 'Generated 1 URL alias.', 'Generated @count URL aliases.'));
+      }
+      else {
+        drupal_set_message(t('No new URL aliases to generate.'));
+      }
+    }
+    else {
+      $error_operation = reset($operations);
+      drupal_set_message(t('An error occurred while processing @operation with arguments : @args', array('@operation' => $error_operation[0], '@args' => print_r($error_operation[0], TRUE))));
+    }
   }
 
 }
