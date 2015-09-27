7f2c30b1a952dd15f1a98f898f2df2f8cd670a26
Sascha Grossenbacher <saschagros@gmail.com>
2015-09-27 17:04:34 +0200

    Update deprecated t() placeholders

--- src/Form/PathautoSettingsForm.php
+++ src/Form/PathautoSettingsForm.php
@@ -102,10 +102,10 @@ class PathautoSettingsForm extends ConfigFormBase {
 
     $description = t('What should Pathauto do when updating an existing content item which already has an alias?');
     if (\Drupal::moduleHandler()->moduleExists('redirect')) {
-      $description .= ' ' . t('The <a href="!url">Redirect module settings</a> affect whether a redirect is created when an alias is deleted.', array('!url' => \Drupal::url('redirect.settings')));
+      $description .= ' ' . t('The <a href=":url">Redirect module settings</a> affect whether a redirect is created when an alias is deleted.', array(':url' => \Drupal::url('redirect.settings')));
     }
     else {
-      $description .= ' ' . t('Considering installing the <a href="!url">Redirect module</a> to get redirects when your aliases change.', array('!url' => 'http://drupal.org/project/redirect'));
+      $description .= ' ' . t('Considering installing the <a href=":url">Redirect module</a> to get redirects when your aliases change.', array('!url' => 'http://drupal.org/project/redirect'));
     }
 
     $form['update_action'] = array(
