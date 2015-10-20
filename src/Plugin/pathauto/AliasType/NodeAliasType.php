d03555533ea9122060a2a3723f263717dfaf1685
Sascha Grossenbacher <saschagros@gmail.com>
2015-10-20 09:26:22 +0200

    Generalize the batchUpdate() method, remove duplicate code

--- src/Plugin/pathauto/AliasType/NodeAliasType.php
+++ src/Plugin/pathauto/AliasType/NodeAliasType.php
@@ -39,48 +39,6 @@ class NodeAliasType extends EntityAliasTypeBase implements AliasTypeBatchUpdateI
   /**
    * {@inheritdoc}
    */
-  public function batchUpdate(&$context) {
-    if (!isset($context['sandbox']['current'])) {
-      $context['sandbox']['count'] = 0;
-      $context['sandbox']['current'] = 0;
-    }
-
-    $query = db_select('node', 'n');
-    $query->leftJoin('url_alias', 'ua', "CONCAT('node/', n.nid) = ua.source");
-    $query->addField('n', 'nid');
-    $query->isNull('ua.source');
-    $query->condition('n.nid', $context['sandbox']['current'], '>');
-    $query->orderBy('n.nid');
-    $query->addTag('pathauto_bulk_update');
-    $query->addMetaData('entity', 'node');
-
-    // Get the total amount of items to process.
-    if (!isset($context['sandbox']['total'])) {
-      $context['sandbox']['total'] = $query->countQuery()->execute()->fetchField();
-
-      // If there are no nodes to update, the stop immediately.
-      if (!$context['sandbox']['total']) {
-        $context['finished'] = 1;
-        return;
-      }
-    }
-
-    $query->range(0, 25);
-    $nids = $query->execute()->fetchCol();
-
-    pathauto_node_update_alias_multiple($nids, 'bulkupdate');
-    $context['sandbox']['count'] += count($nids);
-    $context['sandbox']['current'] = max($nids);
-    $context['message'] = t('Updated alias for node @nid.', array('@nid' => end($nids)));
-
-    if ($context['sandbox']['count'] != $context['sandbox']['total']) {
-      $context['finished'] = $context['sandbox']['count'] / $context['sandbox']['total'];
-    }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function getSourcePrefix() {
     return 'node/';
   }
