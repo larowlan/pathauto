993141a42e30e639cb376421c8ac5ab1819ad617
Kris <kris@meridian-ds.com>
2015-11-09 10:50:19 -0600

    Adding additional handling to the pattern configuration wizard step in order to populate with appropriate tokens from available relationships.

--- src/Form/ConfigurePatternForm.php
+++ src/Form/ConfigurePatternForm.php
@@ -31,6 +31,23 @@ class ConfigurePatternForm extends FormBase {
     $aliasType = $pathauto_pattern->getAliasType();
     $form = $aliasType->buildConfigurationForm($form, $form_state);
     $form['default']['#default_value'] = $pathauto_pattern->getPattern();
+    // This is rough but generally seems right. I think Token is not nuanced
+    // enough to handle this.
+    $tokens = $form['default']['#token_types'];
+    $contexts = $pathauto_pattern->getContexts();
+    foreach ($contexts as $context_id => $context) {
+      list($data_type, $entity_type) = explode(':', $context->getContextDefinition()->getDataType());
+      if ($data_type == 'entity') {
+        if ($entity_type == 'taxonomy_term') {
+          $entity_type = 'term';
+        }
+        if (!in_array($entity_type, $tokens)) {
+          $tokens[] = $entity_type;
+        }
+      }
+    }
+    $form['default']['#token_types'] = $tokens;
+
     return $form;
   }
 
