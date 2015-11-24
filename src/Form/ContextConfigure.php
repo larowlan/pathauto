b1fd07f70e9224dbb4690af40652a60106f9b9f9
Kris <kris@meridian-ds.com>
2015-11-24 15:12:29 -0600

    Abstracting the relationship forms into ctools and rebuilding pathauto implementation as subclasses

--- src/Form/ContextConfigure.php
+++ src/Form/ContextConfigure.php
@@ -6,96 +6,18 @@
 
 namespace Drupal\pathauto\Form;
 
-use Drupal\Core\Ajax\AjaxResponse;
-use Drupal\Core\Ajax\CloseModalDialogCommand;
-use Drupal\Core\Ajax\RedirectCommand;
-use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\Context\Context;
 use Drupal\Core\Plugin\Context\ContextDefinition;
-use Drupal\ctools\TypedDataResolver;
-use Drupal\user\SharedTempStoreFactory;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\ctools\Form\RelationshipConfigure;
 
-class ContextConfigure extends FormBase {
-
-  /**
-   * @var \Drupal\user\SharedTempStoreFactory
-   */
-  protected $tempstore;
-
-  /**
-   * @var \Drupal\ctools\TypedDataResolver
-   */
-  protected $resolver;
-
-  /**
-   * @var string
-   */
-  protected $tempstore_id;
-
-  /**
-   * @var string;
-   */
-  protected $machine_name;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static($container->get('user.shared_tempstore'), $container->get('ctools.typed_data.resolver'));
-  }
-
-  public function __construct(SharedTempStoreFactory $tempstore, TypedDataResolver $resolver) {
-    $this->tempstore = $tempstore;
-    $this->resolver = $resolver;
-  }
+class ContextConfigure extends RelationshipConfigure {
 
   /**
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'ctools_context_configure';
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state, $context = NULL, $tempstore_id = NULL, $machine_name = NULL) {
-    $this->tempstore_id = $tempstore_id;
-    $this->machine_name = $machine_name;
-    $cached_values = $this->tempstore->get($this->tempstore_id)->get($this->machine_name);
-
-    /** @var \Drupal\Core\Plugin\Context\ContextInterface[] $contexts */
-    $contexts = $this->getContexts($cached_values);
-    $context_object = $this->resolver->convertTokenToContext($context, $contexts);
-    $form['id'] = [
-      '#type' => 'value',
-      '#value' => $context
-    ];
-    $form['context_object'] = [
-      '#type' => 'value',
-      '#value' => $context_object,
-    ];
-    $form['context_data'] = [
-      '#type' => 'item',
-      '#title' => $this->resolver->getLabelByToken($context, $contexts),
-      '#markup' => $context_object->getContextDefinition()->getDataType(),
-    ];
-    $form['label'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Context label'),
-      '#default_value' => !empty($contexts[$context]) ? $contexts[$context]->getContextDefinition()->getLabel() : '',
-      '#required' => TRUE,
-    ];
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Save'),
-      '#ajax' => [
-        'callback' => [$this, 'ajaxSave'],
-      ]
-    ];
-    return $form;
+    return 'pathauto_relationship_configure';
   }
 
   /**
@@ -121,59 +43,23 @@ class ContextConfigure extends FormBase {
       $pattern->replaceContext($form_state->getValue('id'), $new_context);
     }
     $this->tempstore->get($this->tempstore_id)->set($this->machine_name, $cached_values);
-    list($route_name, $route_options) = $this->getParentRouteInfo();
-    $form_state->setRedirect($route_name, $route_options);
-  }
-
-  public function ajaxSave(array &$form, FormStateInterface $form_state) {
-    $response = new AjaxResponse();
-    list($route_name, $route_parameters) = $this->getParentRouteInfo();
-    $response->addCommand(new RedirectCommand($this->url($route_name, $route_parameters)));
-    $response->addCommand(new CloseModalDialogCommand());
-    return $response;
+    parent::submitForm($form, $form_state);
   }
 
   /**
-   * Document the route name and parameters for redirect after submission.
-   *
-   * @return array
-   *   In the format of
-   *   return ['route.name', ['machine_name' => $this->machine_name, 'step' => 'step_name']];
+   * {@inheritdoc}
    */
-  protected function getParentRouteInfo() {
+  protected function getParentRouteInfo($cached_values) {
     return ['entity.pathauto_pattern.edit_form', ['machine_name' => $this->machine_name, 'step' => 'contexts']];
   }
 
   /**
-   * @param $context
-   *
-   * @return array
-   *   In the format of
-   *   return ['route.name', ['machine_name' => $this->machine_name, 'context' => $context]];
-   */
-  protected function getRouteInfo($context) {
-    return ['pathauto.pattern.relationship.add', ['machine_name' => $this->machine_name, 'context' => $context]];
-  }
-
-  /**
-   * Custom logic for setting the conditions array in cached_values.
-   *
-   * @param $cached_values
-   *
-   * @param $contexts
-   *   The conditions to set within the cached values.
-   *
-   * @return mixed
-   *   Return the $cached_values
+   * {@inheritdoc}
    */
   protected function setContexts($cached_values, $contexts) {}
 
   /**
-   * Custom logic for retrieving the contexts array from cached_values.
-   *
-   * @param $cached_values
-   *
-   * @return \Drupal\Core\Plugin\Context\ContextInterface[]
+   * {@inheritdoc}
    */
   protected function getContexts($cached_values) {
     /** @var \Drupal\pathauto\PathautoPatternInterface $pattern */
