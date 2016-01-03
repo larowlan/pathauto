561532019a99089fb4b0b4b0b8ea785343e4dc21
Juampy <juampy72@gmail.com>
2016-01-03 20:52:05 +0100

    [#113] Add tests for patterns with language conditions

--- src/Tests/PathautoLocaleTest.php
+++ src/Tests/PathautoLocaleTest.php
@@ -9,7 +9,11 @@ namespace Drupal\pathauto\Tests;
 
 use Drupal\Core\Language\Language;
 use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Plugin\Context\Context;
+use Drupal\Core\Plugin\Context\ContextDefinition;
+use Drupal\Core\Url;
 use Drupal\language\Entity\ConfigurableLanguage;
+use Drupal\language\Entity\ContentLanguageSettings;
 use Drupal\pathauto\PathautoState;
 use Drupal\simpletest\WebTestBase;
 
@@ -27,14 +31,19 @@ class PathautoLocaleTest extends WebTestBase {
    *
    * @var array
    */
-  public static $modules = array('node', 'pathauto', 'locale');
+  public static $modules = array('node', 'pathauto', 'locale', 'content_translation');
 
   /**
-   * Admin user.
-   *
-   * @var \Drupal\user\UserInterface
+   * {@inheritdoc}
    */
-  protected $adminUser;
+  protected function setUp() {
+    parent::setUp();
+
+    // Create Article node types.
+    if ($this->profile != 'standard') {
+      $this->drupalCreateContentType(array('type' => 'article', 'name' => 'Article'));
+    }
+  }
 
   /**
    * Test that when an English node is updated, its old English alias is
@@ -84,5 +93,92 @@ class PathautoLocaleTest extends WebTestBase {
     // suffix.
     $this->assertEntityAlias($node, '/content/english-node-1', LanguageInterface::LANGCODE_NOT_SPECIFIED);
   }
-}
 
+  /**
+   * Test that patterns work on multilingual content.
+   */
+  function testLanguagePatterns() {
+    $this->drupalLogin($this->rootUser);
+
+    // Add French language.
+    ConfigurableLanguage::createFromLangcode('fr')->save();
+
+    // Enable content translation on articles.
+    \Drupal::service('content_translation.manager')->setEnabled('node', 'article', TRUE);
+    drupal_static_reset();
+    \Drupal::entityManager()->clearCachedDefinitions();
+    \Drupal::service('router.builder')->rebuild();
+    \Drupal::service('entity.definition_update_manager')->applyUpdates();
+
+    // Create a pattern for English articles.
+    $pattern = $this->createPattern('node', '/the-articles/[node:title]');
+    $pattern->addSelectionCondition([
+      'id' => 'entity_bundle:node',
+      'bundles' => ['article' => 'article'],
+      'negate' => FALSE,
+      'context_mapping' => ['node' => 'node'],
+    ]);
+    $language_mapping = 'node:langcode:language';
+    $pattern->addSelectionCondition([
+      'id' => 'language',
+      'langcodes' => ['en' => 'en'],
+      'negate' => FALSE,
+      'context_mapping' => ['language' => $language_mapping],
+    ]);
+    $new_definition = new ContextDefinition('language', 'Language');
+    $new_context = new Context($new_definition);
+    $pattern->addContext($language_mapping, $new_context);
+    $pattern->save();
+
+    // Create a pattern for French articles.
+    $pattern = $this->createPattern('node', '/les-articles/[node:title]');
+    $pattern->addSelectionCondition([
+      'id' => 'entity_bundle:node',
+      'bundles' => ['article' => 'article'],
+      'negate' => FALSE,
+      'context_mapping' => ['node' => 'node'],
+    ]);
+    $language_mapping = 'node:langcode:language';
+    $pattern->addSelectionCondition([
+      'id' => 'language',
+      'langcodes' => ['fr' => 'fr'],
+      'negate' => FALSE,
+      'context_mapping' => ['language' => $language_mapping],
+    ]);
+    $new_definition = new ContextDefinition('language', 'Language');
+    $new_context = new Context($new_definition);
+    $pattern->addContext($language_mapping, $new_context);
+    $pattern->save();
+
+    // Create a node and its translation. Assert aliases.
+    $edit = array(
+      'title[0][value]' => 'English node',
+    );
+    $this->drupalPostForm('node/add/article', $edit, t('Save and publish'));
+    $this->drupalGet('node/1/edit');
+    $english_node = $this->drupalGetNodeByTitle('English node');
+    $this->assertAlias('/node/' . $english_node->id(), '/the-articles/english-node', 'en');
+
+    $add_translation_url = Url::fromRoute('entity.node.content_translation_add', ['node' => $english_node->id(), 'source' => 'en', 'target' => 'fr']);
+    $edit = array(
+      'title[0][value]' => 'French node',
+    );
+    $this->drupalPostForm($add_translation_url, $edit, t('Save and keep published (this translation)'));
+    $this->rebuildContainer();
+    $english_node = $this->drupalGetNodeByTitle('English node');
+    $french_node = $english_node->getTranslation('fr');
+    $this->assertAlias('/node/' . $french_node->id(), '/les-articles/french-node', 'fr');
+
+    // Bulk delete and Bulk generate patterns. Assert aliases.
+    $this->deleteAllAliases();
+    // Bulk create aliases.
+    $edit = array(
+      'update[canonical_entities:node]' => TRUE,
+    );
+    $this->drupalPostForm('admin/config/search/path/update_bulk', $edit, t('Update'));
+    $this->assertText('Generated 1 URL alias.');
+    $this->assertAlias('/node/' . $english_node->id(), '/the-articles/english-node', 'en');
+    $this->assertAlias('/node/' . $french_node->id(), '/les-articles/french-node', 'fr');
+  }
+
+}
